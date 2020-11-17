ba3383625e407ecc501c3e464c8547415578f684
“ <1522431127@qq.com>
2020-11-17 09:29:30 +0800

    第一次提交

---
+++ src/test/java/com/clound/jmhshop/JmhshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clound.jmhshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmhshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
